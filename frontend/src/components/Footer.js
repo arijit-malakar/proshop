7ab418d5b1982d1fc897f41a460e58dfa45e6218
Arijit Malakar <arijitmalakar1996@gmail.com>
2023-08-30 16:40:06 +0530

    Added footer styling

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,11 +4,11 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer>
+    <footer className="footer">
       <Container>
         <Row>
-          <Col className="text-center py-3">
-            <p>ProShop &copy; {currentYear}</p>
+          <Col className="text-center py-3 mt-2">
+            <p className="footer-text">E-Mall &copy; {currentYear}</p>
           </Col>
         </Row>
       </Container>
