a6a0632207d7da6f0338e5c3cd99d133c05ccbd4
Arijit Malakar <arijitmalakar1996@gmail.com>
2023-09-02 21:34:01 +0530

    Added banner items fetching and styling for overlay text

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,31 +1,26 @@
-import { Link } from "react-router-dom";
 import { Carousel, Image } from "react-bootstrap";
 import "../assets/styles/ProductCarousel.css";
 import Loader from "./Loader";
 import Message from "./Message";
-import { useGetTopProductsQuery } from "../slices/productsApiSlice";
+import { useGetBannerItemsQuery } from "../slices/bannerApiSlice";
 
 const ProductCarousel = () => {
-  const { data: products, isLoading, error } = useGetTopProductsQuery();
+  const { data, isLoading, error } = useGetBannerItemsQuery();
   return isLoading ? (
     <Loader />
   ) : error ? (
     <Message variant="danger">{error}</Message>
   ) : (
     <Carousel pause="hover" className="mb-4">
-      {/* {products.map((product) => (
-        <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src="images/carousel-img-3.jpg" alt={product.name} fluid />
-            <Carousel.Caption className="carousel-caption">
-              <h2>
-                {product.name} (${product.price})
-              </h2>
-            </Carousel.Caption>
-          </Link>
+      {data.bannerItems.map((banner, i) => (
+        <Carousel.Item key={banner._id}>
+          <Image src={banner.image} alt={`banner-img-${i + 1}`} fluid />
+          <Carousel.Caption>
+            <h2>{banner.caption}</h2>
+          </Carousel.Caption>
         </Carousel.Item>
-      ))} */}
-      {Array.from({ length: 3 }, (_, i) => (
+      ))}
+      {/* {Array.from({ length: 3 }, (_, i) => (
         <Carousel.Item>
           <Image
             src={`images/carousel-img-${i + 1}.jpeg`}
@@ -33,7 +28,7 @@ const ProductCarousel = () => {
             fluid
           />
         </Carousel.Item>
-      ))}
+      ))} */}
     </Carousel>
   );
 };
