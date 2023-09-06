95745dc70c76291b2691605eef305a567c5bac9f
Arijit Malakar <arijitmalakar1996@gmail.com>
2023-09-06 13:24:03 +0530

    Removed unused css and added fonts

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,6 @@ import { HelmetProvider } from "react-helmet-async";
 import { Provider } from "react-redux";
 import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
